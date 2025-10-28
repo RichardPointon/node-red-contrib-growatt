89633dc72c2a0a333727b5f6187faafc3deb2f9f
Richard Pointon <richard@pointon.org.uk>
2025-10-28 17:47:27 +0000

    refactoring

--- nodes/growatt.js
+++ nodes/growatt.js
@@ -1,40 +1,53 @@
 module.exports = function(RED) {
   const api = require('growatt')
 
-  async function pullData(node, config) {
+  async function pullData(node, config, send, done) {
     // Create new API object and login
-    const growatt = new api({})
+    const growatt = new api({});
     node.log(node.confignode.username)
-    let login = await growatt.login(node.confignode.username,node.confignode.password).catch(e => {node.error(e)})
-
-    // If login successfull
-    if (login !== undefined) {
-      // Get data of all plants
-      let options = {
-        plantData: config.optionplantdata,
-        deviceData: config.optiondevicedata,
-        weather: config.optionweather,
-        totalData: config.optiontotaldata,
-        statusData: config.optionstatusdata,
-        deviceType: config.optiondevicetyp,
-        historyLast: config.optionhistorylast,
-        historyAll: config.optionhistoryall
+    let login = await growatt.login(node.confignode.username,node.confignode.password).catch(e => {
+      if (done) {
+        done(e);
       }
-      if (config.optionplantid !== "") {
-        options.plantId = config.optionplantid
-      }
-      let getAllPlantData = await growatt.getAllPlantData(options).catch(e => {node.error(e)})
+    });
 
+    if (login === undefined) {
+      // login failed
+      return;
+    }
+
+    // Get data of all plants
+    let options = {
+      plantData: config.optionplantdata,
+      deviceData: config.optiondevicedata,
+      weather: config.optionweather,
+      totalData: config.optiontotaldata,
+      statusData: config.optionstatusdata,
+      deviceType: config.optiondevicetyp,
+      historyLast: config.optionhistorylast,
+      historyAll: config.optionhistoryall
+    }
+    if (config.optionplantid !== "") {
+      options.plantId = config.optionplantid;
+    }
+    await growatt.getAllPlantData(options)
+    .then((getAllPlantData)=> {
       // Send data as message
       const msg = {
           "_msgid": RED.util.generateId(),
           "payload": getAllPlantData
       }
-      node.send(msg);
-
+      send(msg);
+    })
+    .catch(e => {
+      done(e);
+    })
+    .finally(async ()=> {
       // Logout
-      let logout = await growatt.logout().catch(e => {node.error(e)})
-    }
+      let logout = await growatt.logout().catch(e => {
+        node.error(e);
+      });
+    });
   }
 
   function GrowattNode(config) {
@@ -48,9 +61,9 @@ module.exports = function(RED) {
     }
 
     // Act on incomming messages
-    node.on('input', function(msg) {
+    node.on('input', function(msg, send, done) {
       if (node.confignode != undefined) {
-        pullData(node, config)
+        pullData(node, config, send, done);
       }
     })
   }
